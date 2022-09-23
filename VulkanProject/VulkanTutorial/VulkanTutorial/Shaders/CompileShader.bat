22e0c8c92c6089b5d7d7143187a562f9ec534f40
Stronger111 <776239125@qq.com>
2022-09-23 11:06:24 +0800

    Vulkan深度Buffer
    Vulkan深度Buffer

--- VulkanProject/VulkanTutorial/VulkanTutorial/Shaders/CompileShader.bat
+++ VulkanProject/VulkanTutorial/VulkanTutorial/Shaders/CompileShader.bat
@@ -1,2 +1,3 @@
-E:/Software/VulkanEnv/Bin/glslc.exe 09_shader_base.vert -o 09_shader_base_Vert.spv
+E:/Software/VulkanEnv/Bin/glslc.exe shader_ubo.vert -o shader_ubo.spv
 E:/Software/VulkanEnv/Bin/glslc.exe 09_shader_base.frag -o 09_shader_base_Frag.spv
+pause
