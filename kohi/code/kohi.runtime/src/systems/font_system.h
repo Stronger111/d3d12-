280f13835509bae77ca526c7eee4a71cab670516
Stronger111 <776239125@qq.com>
2025-07-14 18:27:38 +0800

    Converted font system to load from app config

--- kohi/code/kohi.runtime/src/systems/font_system.h
+++ kohi/code/kohi.runtime/src/systems/font_system.h
@@ -5,41 +5,44 @@
 #include "resources/resource_types.h"
 
 typedef struct system_font_config {
-    char* name;
+    /** @brief The name of the font. */
+    const char* name;
+    /** @brief The default size of the font. */
     u16 default_size;
-    char* resource_name;
+    /** @brief The name of the resource containing the font data. */
+    const char* resource_name;
 } system_font_config;
 
 typedef struct bitmap_font_config {
-    char* name;
+    /** @brief The name of the font. */
+    const char* name;
     u16 size;
+    /** @brief The name of the resource containing the font data. */
     char* resource_name;
 } bitmap_font_config;
 
 typedef struct font_system_config {
-    u8 default_system_font_count;
-    system_font_config* system_font_configs;
-    u8 default_bitmap_font_count;
-    bitmap_font_config* bitmap_font_configs;
-    u8 max_system_font_count;
-    u8 max_bitmap_font_count;
+     /** @brief The default system font config. */
+    system_font_config default_system_font;
+    /** @brief The default bitmap font config. */
+    bitmap_font_config default_bitmap_font;
     b8 auto_release;
 } font_system_config;
 
-struct ui_text;
+b8 font_system_deserialize_config(const char* config_str, font_system_config* out_config);
 
-b8 font_system_initialize(u64* memory_requirement, void* memory, void* config);
+b8 font_system_initialize(u64* memory_requirement, void* memory, font_system_config* config);
 void font_system_shutdown(void* memory);
 /**
  * @brief Loads a system font from the following config.
- * 
+ *
  * @param config A pointer to the config to use for loading.
  * @return True on success; otherwise false.
  */
 KAPI b8 font_system_system_font_load(system_font_config* config);
 /**
  * @brief Loads a bitmap font from the following config.
- * 
+ *
  * @param config A pointer to the config to use for loading.
  * @return True on success; otherwise false.
  */
@@ -63,7 +66,7 @@ KAPI font_data* font_system_acquire(const char* font_name, u16 font_size, font_t
  */
 KAPI b8 font_system_release(struct ui_text* text);
 //校验图集
-KAPI b8 font_system_verify_atlas(font_data* font,const char* text);
+KAPI b8 font_system_verify_atlas(font_data* font, const char* text);
 
 /**
  * @brief Measures the given string to find out how large it is at the widest/tallest point.
@@ -71,4 +74,4 @@ KAPI b8 font_system_verify_atlas(font_data* font,const char* text);
  * @param font A pointer to the font to use for measuring.
  * @param text The text to be measured.
  */
-KAPI vec2 font_system_measure_string(font_data* font,const char* text);
+KAPI vec2 font_system_measure_string(font_data* font, const char* text);
